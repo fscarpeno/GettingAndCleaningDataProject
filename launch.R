83a6963c2abbc824054e56a8bedc58bd2afc0448
fscarpeno <fscarpeno@gmail.com>
2016-11-29 12:38:35 +0100

    Update launch.R

--- launch.R
+++ launch.R
@@ -5,5 +5,5 @@ pathFile <- "C:/workspace/R/Clean/week4/Ex/run_analysis.R"
 source(pathFile)
 runAnalysis("C:/workspace/R/Clean/week4/Ex/UCI HAR Dataset",
 		"C:/workspace/R/Clean/week4/Ex/output.csv",
-		"C:/workspace/R/Clean/week4/Ex/codebook.csv",
+		"C:/workspace/R/Clean/week4/Ex/CodeBook.md",
 		",")
